d6a622afe6d3926b0edaa128389ee17e01968c09
KevinNivekReal <47750721+KevinNivekReal@users.noreply.github.com>
2024-01-12 22:22:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ let angle = 180;
 function openMenu() {
     rotation = (rotation + angle) %360;
     carrot.style.transform = "rotate(" + rotation + "deg)";
-    if (rotation == 180) {
-        navbar.style.display ="block";
+    if (rotation == 180) {     
+        
+        navbar.style.display ="flex";
+        setTimeout(() => {
+            navbar.style.opacity = 1;
+        }, this.animationDelay + 20);
+        document.getElementsByClassName("body")[0].style.opacity = .5;   
     } else {
-        navbar.style.display = "none";
+        
+        setTimeout(() => {
+            navbar.style.opacity = 0;
+            
+        }, this.animationDelay + 20);   
+        document.getElementsByClassName("body")[0].style.opacity = 1; 
+        setTimeout(() => {
+            navbar.style.display = "none";
+        }, 500); 
+        
+        
     }
 }
