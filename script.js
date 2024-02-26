8b623508b0ef118a07ff2ac46943a95368d738cf
KevinNivekReal <47750721+KevinNivekReal@users.noreply.github.com>
2024-02-25 19:48:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*This is for the navigation bar*/
+// handles opening the nav bar menu and turning the carrot
 let carrot = document.getElementById("carrot");
 let navbar = document.getElementById("navbar");
 let rotation = 0;
@@ -30,14 +30,53 @@ function openMenu() {
     }
 }
 
-/*This is for the textarea in the contact page*/
+//handles the search feature
 
+var tag = document.getElementById("search");
+var items = document.getElementsByClassName("item");
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    let input = document.getElementById("input-join-name")
-    input.addEventListener('input', function() {
-    document.getElementById("join-name").innerHTML = this.value;
-    },)
-});
+var list = ["accounting", "alyssa flores", "amanda tremaine", "beki treadway","bert bronaugh","britney crowe", "calculator applications","christine staab", "computer programming", "computer science written test","copy editing","current issues and events", "debra sanderson","devon robinson","editorial writing","feature writing", "headline writing","jude john", "literary criticism","mathematics", "news writing", "number sense","one act play", "ready writing","robotics","science","social studies","speech and debate","spelling"];
 
+function showSearch() {
+    var container = document.getElementById("search-container");
+    if (container.style.display == "block"){
+        container.style.display = "none";
+    } else {
+        container.style.display = "block";
+    }
+}
+
+function changed() {
+  var value = tag.value.toLowerCase();
+  var length = value.length;
+  for (var i = 0; i < list.length; i++){
+  var element = document.getElementById(list[i]);
+  var inner = element.innerHTML.trim().toLowerCase();
+  if (value == ""){
+  	element.innerHTML = list[i].charAt(0).toUpperCase() + list[i].slice(1);
+    element.style.display = "block";
+  } else{
+  	if (list[i].toLowerCase().includes(value.toLowerCase())){
+    	element.style.display = "block";
+      
+      var newVal = list[i].charAt(0).toLowerCase() == (value.charAt(0)) ? value.charAt(0).toUpperCase() + value.slice(1) : value;
+      
+      var string = list[i].substring(0,list[i].indexOf(value)) + ("<span class='item-letter'>"+ newVal +"</span>") + list[i].substring(list[i].indexOf(value) + value.length,list[i].length);
+      
+      element.innerHTML = string.charAt(0).toUpperCase() + string.slice(1);
+      //element.innerHTML = inner.substring(0,inner.indexOf(value)) + ("<span class='item-letter'>"+ newVal +"</span>") + inner.substring(inner.indexOf(value) + value.length,inner.length);
+      //element.innerHTML.replace(regex, "<span class='item-letter'> " + value + " </span");
+    }
+    else {
+    	element.style.display = "none";
+      element.innerHTML = "";
+      /*var underline = document.getElementById(list[i] + " underline");
+      if (element.contains(underline) != null){
+      	element.removeChild(underline);
+      }*/
+      
+    }
+   }
+  }
+}
 
